54dfa2528699e589cfbd62b9f58114a854d18684
Mark McAdam <mark.mcadam@genesys.com>
2025-06-25 03:26:59 +0100

    cant find assets

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/inab-app/",
+  base: "/",
   plugins: [react()],
   server: {
     hmr: true,
